762de5c306c249182a2b0d4bc6e722e32fd28877
Albin Geo <albingeov@gmail.com>
2020-12-17 19:58:18 +0530

    Domain Model Classes are added @.Domain

---
+++ MessEssence.Domain/EmployeeDomainModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessEssence.Domain
+{
+    public class EmployeeDomainModel
+    {
+
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public Nullable<int> DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
