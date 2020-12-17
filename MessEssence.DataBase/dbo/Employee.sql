2793f2cfce0453bdeba8516c0c27bceb80f35d4c
Albin Geo <albingeov@gmail.com>
2020-12-17 18:16:30 +0530

    Modified Database Layer

--- MessEssence.DataBase/dbo/Employee.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Employee]
-(
-	[EmployeeId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [EmployeeName] VARCHAR(50) NULL, 
-    [DepartmentId] INT NOT NULL CONSTRAINT [FK_Employee_Department] FOREIGN KEY (DepartmentId) REFERENCES [Department]([DepartmentId]), 
-    
-)
