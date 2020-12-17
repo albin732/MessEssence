01c44c13c24303c3b73d5bd5d8becbda66f222ac
Albin Geo <albingeov@gmail.com>
2020-12-17 19:12:51 +0530

    Defined Generic Interface and its implimentation  ( UnitOfWork and BaseRepository) @.Repository

---
+++ MessEssence.Repository/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using MessEssence.Repository.Infrastructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessEssence.Repository.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MessEssenceSQLDbEntities _dbContext;
+
+        public UnitOfWork()
+        {
+            _dbContext = new MessEssenceSQLDbEntities();
+        }
+
+        public DbContext Db
+        {
+            get { return _dbContext; }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
