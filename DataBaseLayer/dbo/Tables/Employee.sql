99aafd501c17320e9afb7c2252b4888bf9ae8b79
Albin Geo <albingeov@gmail.com>
2020-12-18 08:51:55 +0530

    Created new DBLayer,published Created edmx

---
+++ DataBaseLayer/dbo/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[EmployeeId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NULL, 
+    [DepartmentId] INT NOT NULL, 
+    [Address] NVARCHAR(50) NULL, 
+    [IsDeleted] BIT NULL, 
+    CONSTRAINT [FK_Employee_Department] FOREIGN KEY ([DepartmentId]) REFERENCES [Department]([DepartmentId])
+)
